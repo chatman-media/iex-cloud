39c5395cfb9bd5cb396fed1a38e9048433840846
Alexander Kireev <farvistadw@gmail.com>
2020-02-03 01:28:02 +0700

    removed logs

--- src/endpoints/market-info/SectorPerformance.test.ts
+++ src/endpoints/market-info/SectorPerformance.test.ts
@@ -3,8 +3,6 @@ import { sectorPerformance } from './index';
 describe('#sectorPerformance', () => {
   test('call', async () => {
     const result = await sectorPerformance();
-    console.log(result);
-
     expect(result?.length).toBeGreaterThan(0);
   });
 });
