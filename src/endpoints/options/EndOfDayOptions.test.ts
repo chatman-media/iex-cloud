8c85d326e7f06e642fc3f35b18c5b68f7f795857
Ryan Tracey <ryan.m.tracey@gmail.com>
2020-10-27 11:53:03 -0400

    test(endOfDayOptions): remove console.dir()

--- src/endpoints/options/EndOfDayOptions.test.ts
+++ src/endpoints/options/EndOfDayOptions.test.ts
@@ -3,7 +3,6 @@ import { endOfDayOptions } from './index';
 describe('#endOfDayOptions', () => {
   test('call', async () => {
     const result = await endOfDayOptions('AAPL');
-    console.dir(result);
     expect(result).not.toEqual(null);
   });
 });
