7921f9f47241199147e0a88a25c0bfc719b7d1d4
Ryan Tracey <ryan.m.tracey@gmail.com>
2020-10-29 12:15:05 -0400

    refactor(endpoints): update options.endOfDayOptions() to handle full response

--- src/endpoints/options/EndOfDayOptions.ts
+++ src/endpoints/options/EndOfDayOptions.ts
@@ -1,11 +1,45 @@
 import { ApiRequest } from '../../core';
-import { OptionsSymbol } from '../reference-data'
 
 /**
  * [End of Day Options](https://iexcloud.io/docs/api/#end-of-day-options)
- * This call returns an array of available contract dates for the given symbol.
+ * This call returns an array of OptionResponse for the given symbol.
  */
-export const endOfDayOptions = (symbol: string): Promise<readonly OptionsSymbol[]> => {
+export const endOfDayOptions = (
+  optionsParams: OptionsParams,
+): Promise<readonly OptionResponse[]> => {
+  const { expiration, optionSide, symbol } = optionsParams;
+
+  if (expiration) {
+    return ApiRequest(`stock/${symbol}/options/${expiration}/${optionSide}`);
+  }
+
   return ApiRequest(`stock/${symbol}/options`);
 };
 
+export interface OptionsParams {
+  readonly symbol: String;
+  readonly expiration?: String;
+  readonly optionSide?: OptionSide;
+}
+
+export interface OptionResponse {
+  readonly symbol?: String;
+  readonly id?: String;
+  readonly expirationDate: String;
+  readonly contractSize?: Number;
+  readonly strikePrice?: Number;
+  readonly closingPrice?: Number;
+  readonly side?: OptionSide;
+  readonly type?: String;
+  readonly volume?: Number;
+  readonly openInterest?: Number;
+  readonly bid?: Number;
+  readonly ask?: Number;
+  readonly lastUpdated?: String;
+  readonly isAdjusted?: Boolean;
+}
+
+export enum OptionSide {
+  call = 'call',
+  put = 'put',
+}
